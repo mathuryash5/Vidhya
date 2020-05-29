74eb21d7a2782a517f874c2358d6e805c26863a5
DeltaForx <ishamohano97@gmail.com>
2020-05-30 05:29:47 +0530

    Fetch data from Gremlin API

--- blueprint/routing.py
+++ blueprint/routing.py
@@ -2,8 +2,11 @@ from html import unescape
 
 from flask import Blueprint, render_template, request
 
+from gremlin.azure_gremlin import MicrosoftAzureCosmosDBGremlinAPI
 from util.format import FormatUtils
 from util.model import ModelUtils
+
+import pandas
 
 vidhya_api = Blueprint('vidhya_api', __name__)
 
@@ -13,9 +16,9 @@ def get_homepage():
     return render_template('index.html')
 
 @vidhya_api.route('/knowledge-graph-system/', methods=["GET"])
-@vidhya_api.route('/knowledge-graph-system/search/', methods=["GET"])
 def get_knowledge_graph_system():
-    return render_template('index.html')
+    graph, keys = MicrosoftAzureCosmosDBGremlinAPI.get_knowledge_graph()
+    return render_template('index.html', graph=graph, keys=keys)
 
 @vidhya_api.route('/paper-recommender-system/', methods=["GET"])
 @vidhya_api.route('/paper-recommender-system/search/', methods=["GET"])
@@ -41,6 +44,7 @@ def get_paper_recommeder_system_search_result():
     df = ModelUtils.get_similar_papers_wrappers(user_input_search_string, title_weights=user_title_weight,
                                                  abstract_weights=1-user_title_weight,
                                                 number_of_similar_papers=user_input_number_of_results)
+    pandas.set_option('display.max_colwidth', 400)
     df = FormatUtils.paper_recommender_system_formatter(df)
     paper_recommender_search_result = df.to_html(table_id="table", classes=["table", "table-bordered", "table-light", "table-hover", "table-striped"],
     index=False)
@@ -56,10 +60,12 @@ def get_q_a_system_search_result():
     user_input_search_string = user_input["q-a-system-user-input-string"]
     user_input_number_of_results = int(user_input["q-a-system-user-input-result-count"])
     df = ModelUtils.get_answer_similarity(user_input_search_string, user_input_number_of_results)
-    q_a_search_result = df.to_html(table_id="table",
+    df = FormatUtils.paper_recommender_system_formatter(df)
+    q_a_search_result = df.to_html(table_id="table-1",
                                                  classes=["table", "table-bordered", "table-light", "table-hover",
                                                           "table-striped"],
                                                  index=False)
+    q_a_search_result = unescape(q_a_search_result)
     if user_input_search_string.strip() == "":
         return render_template("intex.html")
     else:
